7fcc11bd7f8723f61f33ec0616a646e1340155a4
chrs <christaudt@gmx.net>
2013-10-21 23:13:33 +0200

    erster Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RealDemo::Application.config.secret_key_base = '2cc5311d463d00acd84fcfacc005c034d8a8657b646eac76ad5e39da5a5db8a07a69cdbcb51c1475a92d6655e84df8ddac21b21bab9939aabc869f37dbbb7648'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RealDemo::Application.config.session_store :cookie_store, key: '_real-demo_session'
